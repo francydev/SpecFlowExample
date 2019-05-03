b10bcd2e0be456af357cebe8fc76a44312396eff
Francesco de Vicariis <francesco.devicariis@blexin.com>
2019-05-03 11:58:07 +0200

    Push iniziale

---
+++ SpecFlowExample/ItemsSteps.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowExample
+{
+    [Binding]
+    public class ItemsSteps
+    {
+        [Given(@"the following specific Items file")]
+        public void GivenTheFollowingSpecificItemsFile(string multilineText)
+        {
+            File.WriteAllText(
+                Path.Combine("c:\temp", $"TEST_items.csv"),
+                multilineText);
+        }
+        
+        [When(@"the service is started")]
+        public void WhenTheServiceIsStarted()
+        {
+            var test = true;
+        }
+        
+        [Then(@"the following records must be present in Items table")]
+        public void ThenTheFollowingRecordsMustBePresentInItemsTable(Table table)
+        {
+            Assert.IsTrue(true);   
+        }
+    }
+}
